001f74e8af6d42236dd3d738a84e9d372fc3919d
mfham <mfhamgithub@gmail.com>
2016-08-06 11:21:01 +0900

    remove waste statement

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -11,17 +11,4 @@ user_id: {!! Form::text('user_id'); !!}
 {!! Form::close() !!}
 
 result
-    <a href=http://192.168.56.201/cgi-bin/mfham/grn.cgi/schedule/add?bdate=2016-08-07&uid=67>link</a>
-
-
-    <form name="schedule/add" id="schedule/add" method="post" action="/cgi-bin/mfham/grn.cgi/schedule/command_add?">
-    <input type='hidden' name='tab_name' value="schedule/add">
-        <input type="hidden" name="tmp_key" value="1470430261">
-        <input type="hidden" name="allow_file_attachment" value="1">
-        
-    <div class="button">
-    <button type="submit">Send your message</button>
-    </div>
-    </form>
-    </body>
 </html>
